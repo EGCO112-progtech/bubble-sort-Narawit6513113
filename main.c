e0a8fe955f4ac860e87dd7ff7a60e6a1136eb606
narawit6513113 <narawit.pha@student.mahidol.edu>
2023-01-27 17:01:37 +0700

    chang smth

--- main.c
+++ main.c
@@ -1,18 +1,34 @@
 #define N 6
 #include <stdio.h>
-
+#include <stdlib.h>
 #include "sorting.h"
 
-int main() {
-  int a[N]={3,2,6,7,3,1};
-  int i,j,new_number;
+int main(int argc , char **argv) {
 
-   display(a,N);
-  bubbleSort(a,N); 
+  int *a;
+  int n=argc-1;
+  a = (int*)malloc(sizeof(int)*n);
+  int i,j = 0,new_number;
+  int *prime=(int*)malloc(sizeof(int)*n);
+  for (i = 0 ; i < n ; i++)
+  {
+    a[i] = atoi(argv[i+1]);
+    if (Isprime(a[i]) == 1)
+    {
+      prime[j]=a[i];
+      j++;
+    }
+  }
+  n = j;
+  if (n == 0) printf("No prime\n");
+  else{
+    display(prime,n);
+    bubbleSort(prime,n); 
    //insertion(a,N);
 
   // selectionSort(a,N);
-   display(a,N);
+    display(prime,n);
+  }
  return 0;
 }
 
--- sorting.h
+++ sorting.h
@@ -34,18 +34,45 @@ void swap(int *a,int *b){
   
 }
 
-void bubbleSort(int a[],int n){
-
-int i,j;
-int sorted;
-// how may pair to compare?
-for(j=1;j<=n-1;j++){
- 
-  display(a,n);
- }
-  
+void bubbleSort(int a[],int n)
+{
+  int i,j;
+  // how may pair to compare?
+  for(i = 0 ; i < n-1 ; i++)
+  {
+    int sorted;
+    printf("round %d\n",i+1);
+    for(j = 0 ; j <= n-1 ; j++)
+    {
+      if (a[j] > a[j+1])
+      {
+        swap(&a[j] , &a[j+1]);
+        sorted = 1;
+      }
+      display(a,n);
+    }
+    if (sorted == 0) break;
+  } 
 }
 
+int Isprime(int z)
+{
+  int n, i, flag = 0;
+  if (z == 0 || z == 1)
+    flag = 1;
+
+  for (i = 2; i <= z / 2; ++i) {
+
+    if (z % i == 0) {
+      flag = 1;
+      break;
+    }
+  }
+
+  // flag is 0 for prime numbers
+  if (flag == 0)
+    return 1;
+}
 
 
 
